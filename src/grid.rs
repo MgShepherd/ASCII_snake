8a199b85e21a9b9a60aebdb19bdf0767a734e840
MgShepherd <michaelshep1504@gmail.com>
2025-04-14 19:01:21 +0100

    Added apple to grid and collision between snake and apple

--- src/grid.rs
+++ src/grid.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use std::fmt;
 
 use crate::input_handler::Direction;
@@ -12,20 +13,24 @@ pub struct Grid {
     pub width: u32,
     pub height: u32,
     snake_position: Position,
+    apple_position: Position,
 }
 
 impl Grid {
     //TODO: Handle negative grid sizes
     pub fn new(width: u32, height: u32) -> Self {
-        let starting_position = Position {
+        let snake_position = Position {
             x: width / 2,
             y: height / 2,
         };
 
+        let apple_position = get_apple_position(width, height, &snake_position);
+
         Grid {
             width,
             height,
-            snake_position: starting_position,
+            snake_position,
+            apple_position,
         }
     }
 
@@ -37,7 +42,15 @@ impl Grid {
             Direction::Left => self.snake_position.x -= 1,
             Direction::Right => self.snake_position.x += 1,
         }
+        self.check_apple_collision();
         self.is_snake_location_valid()
+    }
+
+    fn check_apple_collision(&mut self) {
+        if self.snake_position == self.apple_position {
+            self.apple_position = get_apple_position(self.width, self.height, &self.snake_position);
+            // TODO: Increase length of the snake when apple collected
+        }
     }
 
     fn is_snake_location_valid(&self) -> bool {
@@ -58,6 +71,8 @@ impl fmt::Display for Grid {
                     write!(f, "#")?;
                 } else if self.snake_position == (Position { x, y }) {
                     write!(f, "■")?;
+                } else if self.apple_position == (Position { x, y }) {
+                    write!(f, "A")?;
                 } else {
                     write!(f, " ")?;
                 }
@@ -66,4 +81,16 @@ impl fmt::Display for Grid {
         }
         Ok(())
     }
+}
+
+fn get_apple_position(width: u32, height: u32, snake_position: &Position) -> Position {
+    let mut apple_position = Position {
+        x: snake_position.x,
+        y: snake_position.y,
+    };
+    while apple_position == *snake_position {
+        apple_position.x = rand::rng().random_range(1..(width - 1));
+        apple_position.y = rand::rng().random_range(1..(height - 1));
+    }
+    apple_position
 }
