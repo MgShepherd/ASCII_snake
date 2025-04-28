bb9983f6728fd351702f05f5773e17195beda5e7
MgShepherd <michaelshep1504@gmail.com>
2025-04-28 08:22:24 +0100

    Added functionality for snake to grow in size when apple eaten

--- src/grid.rs
+++ src/grid.rs
@@ -4,7 +4,7 @@ use std::fmt;
 use crate::direction::Direction;
 use colored::Colorize;
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Copy)]
 struct Position {
     x: u32,
     y: u32,
@@ -13,52 +13,79 @@ struct Position {
 pub struct Grid {
     pub width: u32,
     pub height: u32,
-    snake_position: Position,
+    snake_positions: Vec<Position>,
     apple_position: Position,
 }
 
 impl Grid {
     //TODO: Handle negative grid sizes
     pub fn new(width: u32, height: u32) -> Self {
-        let snake_position = Position {
+        let snake_positions = vec![Position {
             x: width / 2,
             y: height / 2,
-        };
+        }];
 
-        let apple_position = get_apple_position(width, height, &snake_position);
+        let apple_position = get_apple_position(width, height, &snake_positions);
 
         Grid {
             width,
             height,
-            snake_position,
+            snake_positions,
             apple_position,
         }
     }
 
     pub fn update(&mut self, current_direction: &Direction) -> bool {
-        //TODO: Stop snake from being able to go back on itself
-        match current_direction {
-            Direction::Up => self.snake_position.y -= 1,
-            Direction::Down => self.snake_position.y += 1,
-            Direction::Left => self.snake_position.x -= 1,
-            Direction::Right => self.snake_position.x += 1,
-        }
-        self.check_apple_collision();
+        let new_position = self.get_next_snake_pos(current_direction);
+        // TODO: Snake collision with itself
+        self.check_apple_collision(&new_position);
+        self.update_snake_positions(&new_position);
         self.is_snake_location_valid()
     }
 
-    fn check_apple_collision(&mut self) {
-        if self.snake_position == self.apple_position {
-            self.apple_position = get_apple_position(self.width, self.height, &self.snake_position);
-            // TODO: Increase length of the snake when apple collected
+    fn get_next_snake_pos(&self, current_direction: &Direction) -> Position {
+        match current_direction {
+            Direction::Up => Position {
+                x: self.snake_positions[0].x,
+                y: self.snake_positions[0].y - 1,
+            },
+            Direction::Down => Position {
+                x: self.snake_positions[0].x,
+                y: self.snake_positions[0].y + 1,
+            },
+
+            Direction::Left => Position {
+                x: self.snake_positions[0].x - 1,
+                y: self.snake_positions[0].y,
+            },
+
+            Direction::Right => Position {
+                x: self.snake_positions[0].x + 1,
+                y: self.snake_positions[0].y,
+            },
+        }
+    }
+
+    fn check_apple_collision(&mut self, new_position: &Position) {
+        if *new_position == self.apple_position {
+            self.apple_position =
+                get_apple_position(self.width, self.height, &self.snake_positions);
+            self.snake_positions.push(Position { x: 0, y: 0 });
         }
     }
 
     fn is_snake_location_valid(&self) -> bool {
-        self.snake_position.x > 0
-            && self.snake_position.y > 0
-            && self.snake_position.x < self.width - 1
-            && self.snake_position.y < self.height - 1
+        self.snake_positions[0].x > 0
+            && self.snake_positions[0].y > 0
+            && self.snake_positions[0].x < self.width - 1
+            && self.snake_positions[0].y < self.height - 1
+    }
+
+    fn update_snake_positions(&mut self, new_position: &Position) {
+        for i in (1..self.snake_positions.len()).rev() {
+            self.snake_positions[i] = self.snake_positions[i - 1];
+        }
+        self.snake_positions[0] = *new_position;
     }
 }
 
@@ -70,7 +97,7 @@ impl fmt::Display for Grid {
             for x in 0..self.width {
                 if y == 0 || y == self.height - 1 || x == 0 || x == self.width - 1 {
                     write!(f, "{}", "#".red())?;
-                } else if self.snake_position == (Position { x, y }) {
+                } else if self.snake_positions.contains(&(Position { x, y })) {
                     write!(f, "{}", "■".blue())?;
                 } else if self.apple_position == (Position { x, y }) {
                     write!(f, "{}", "A".green())?;
@@ -84,12 +111,12 @@ impl fmt::Display for Grid {
     }
 }
 
-fn get_apple_position(width: u32, height: u32, snake_position: &Position) -> Position {
+fn get_apple_position(width: u32, height: u32, snake_positions: &Vec<Position>) -> Position {
     let mut apple_position = Position {
-        x: snake_position.x,
-        y: snake_position.y,
+        x: snake_positions[0].x,
+        y: snake_positions[0].y,
     };
-    while apple_position == *snake_position {
+    while snake_positions.contains(&apple_position) {
         apple_position.x = rand::rng().random_range(1..(width - 1));
         apple_position.y = rand::rng().random_range(1..(height - 1));
     }
